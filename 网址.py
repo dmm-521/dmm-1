b73dbb98543c5183b6a02047b49ddd34da343902
dmm-521 <53457097+dmm-521@users.noreply.github.com>
2019-08-02 14:19:30 +0800

    Add files via upload

---
+++ 网址.py
@@ -0,0 +1,35 @@
+a='<li><a href="http://www.4399dmw.com/haizeiwang/manhua/274923.html">海贼王826话 山治家族姐弟登场</a></li>'
+b='<li><a href="http://www.4399dmw.com/haizeiwang/shipin/271774.html">海贼王燃烧之血超震撼游戏宣传片</a></li>'
+c='<li><a href="http://www.4399dmw.com/haizeiwang/tupian/270215.html">海贼王第80卷SBS新鲜内容大放送！</a></li>'
+d='<li><a href="http://www.4399dmw.com/haizeiwang/donghua/270363.html">海贼王730集  蔓雪莉的奇迹之泪</a></li>'
+e='<li><a href="http://www.4399dmw.com/haizeiwang/juqing/262935.html">50条关于海贼王的小秘密 你都知道哪一些</a></li>'
+f='<li><a href="http://www.4399dmw.com/haizeiwang/shipin/261621.html">尾田荣一郎现场彩页作画过程</a></li>'
+g='<li><a href="http://www.4399dmw.com/haizeiwang/tupian/260698.html">海贼王2016年动画版年历</a></li>'
+h='<li><a href="http://www.4399dmw.com/haizeiwang/tupian/260371.html">海贼王2016年官方月历欣赏</a></li>'
+i='<li><a href="http://weibo.com/xiaduophoto">关注夏多老师微博  实时交流海贼王之道！</a></li>'
+j='<li>4399海贼王官方群 群号：183253772</li>'
+k='<li><a href="http://www.4399dmw.com/haizeiwang/juqing/220490.html">夏多分析合集 过去的篇章全部在这里</a></li>'
+l='<li><a href="http://www.4399dmw.com/tuku/269004.html">海贼王burning blood参战角色图赏</a></li>'
+m='<li>4399海贼王官方超级群 QQ群号：76975076</li>'
+n='<li><a href="http://weibo.com/dm4399">4399动漫网官方微博 萌萌微博娘等你来调戏</a></li>'
+o='<li>海贼剧情讨论的乐园 夏多海贼团群号：385212372</li>'
+import re
+compile_ = re.compile('"(http.*)"')
+ra = compile_.findall(a)
+rb = compile_.findall(b)
+rc = compile_.findall(c)
+rd = compile_.findall(d)
+re = compile_.findall(e)
+rf = compile_.findall(f)
+rg = compile_.findall(g)
+rh = compile_.findall(h)
+ri = compile_.findall(i)
+rj = compile_.findall(j)
+rk = compile_.findall(k)
+rl = compile_.findall(l)
+rm = compile_.findall(m)
+rn = compile_.findall(n)
+ro = compile_.findall(o)
+print(ra,rb,rc,rd,re,rf,rg,rh,ri,rj,rk,rl,rm,rn,ro,sep="\n")
+
+
